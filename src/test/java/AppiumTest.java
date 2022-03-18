5cc65b61b62b65dfd8baec98e389453c91168924
Your Name <akcaalanvolkan@gamil.com>
2022-03-18 13:26:06 -0400

    first commit

---
+++ src/test/java/AppiumTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void NativeAPpAndroid()
+    {
+        System.out.println("NativeAPpAndroid");
+    }
+
+    @Test
+    public void IOSApps()
+    {
+        System.out.println("IOSApps");
+    }
+}
---
+++ src/test/java/RESTAPITest.java
@@ -0,0 +1,17 @@
+
+import org.testng.annotations.Test;
+//rahulonlinetutor@gmail.com
+public class RESTAPITest {
+
+    @Test
+    public void postJira()
+    {
+        System.out.println("postJira");
+    }
+
+    @Test
+    public void deleteTwitter()
+    {
+        System.out.println("deleteTwitter");
+    }
+}
